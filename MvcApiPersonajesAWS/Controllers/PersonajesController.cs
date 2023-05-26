de001126e63a4954ce5c390c546c3357768ba8aa
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-05-26 10:38:07 +0200

    mvc

---
+++ MvcApiPersonajesAWS/Controllers/PersonajesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiPersonajesAWS.Services;
+
+namespace MvcApiPersonajesAWS.Controllers
+{
+    public class PersonajesController : Controller
+    {
+        private readonly ServiceApiPersonajes apiPersonajes;
+
+        public PersonajesController(ServiceApiPersonajes apiPersonajes)
+        {
+            this.apiPersonajes = apiPersonajes;
+        }
+
+        public IActionResult ApiPersonajes()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ApiEC2Personajes()
+        {
+            return View(await this.apiPersonajes.GetPersonajesEC2Async());
+        }
+    }
+}
---
+++ MvcApiPersonajesAWS/Services/ServiceApiPersonajes.cs
@@ -0,0 +1,67 @@
+﻿using MvcApiPersonajesAWS.Models;
+
+namespace MvcApiPersonajesAWS.Services
+{
+    public class ServiceApiPersonajes
+    {
+        private string UrlApi;
+        private string UrlApiEC2;
+        private readonly IHttpClientFactory HttpClientFactory;
+
+        public ServiceApiPersonajes(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            this.UrlApi = configuration.GetValue<string>("ApiUrls:ApiPersonajes");
+            this.UrlApiEC2 = configuration.GetValue<string>("ApiUrls:ApiPersonajesEC2");
+            HttpClientFactory = httpClientFactory;
+        }
+
+        private async Task<T?> CallApiAsync<T>(string urlApi, string request)
+        {
+            using HttpClient httpClient = HttpClientFactory.CreateClient();
+
+            httpClient.BaseAddress = new Uri(urlApi);
+            var response = await httpClient.GetAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                return default;
+            }
+
+            return await response.Content.ReadAsAsync<T>();
+        }
+
+        private async Task<T?> CallApiNoSSLAsync<T>(string urlApi, string request)
+        {
+
+            var handler = new HttpClientHandler
+            {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                ServerCertificateCustomValidationCallback =
+                (httpRequestMessage, cert, cetChain, policyErrors) =>
+                {
+                    return true;
+                }
+            };
+
+            var httpClient = new HttpClient(handler);
+
+            httpClient.BaseAddress = new Uri(urlApi);
+            var response = await httpClient.GetAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                return default;
+            }
+
+            return await response.Content.ReadAsAsync<T>();
+        }
+
+        public async Task<List<Personaje>?> GetPersonajesAsync()
+        {
+            return await this.CallApiAsync<List<Personaje>>(this.UrlApi, "/api/personajes");
+        }
+
+        public async Task<List<Personaje>?> GetPersonajesEC2Async()
+        {
+            return await this.CallApiNoSSLAsync<List<Personaje>>(this.UrlApiEC2, "/api/personajes");
+        }
+    }
+}
